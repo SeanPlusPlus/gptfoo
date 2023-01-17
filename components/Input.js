cf184a31a202def62a00716b76971993bf3c25e7
Sean Stephenson <sean.m.stephenson@disney.com>
2023-01-17 11:47:51 -0800

    main: btn

--- components/Input.js
+++ components/Input.js
@@ -3,14 +3,25 @@ import { GlobalContext } from '../context/GlobalState'
 
 export default function Input() {
   const {
-    setInputText
+    inputText,
+    setInputText,
   } = useContext(GlobalContext)
 
   const handleChange = (e) => {
     setInputText(e.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputText)
+  }
+
   return (
-    <textarea className="textarea textarea-bordered w-80 md:w-[32rem]" placeholder="Enter text here" onChange={(e) => handleChange(e)}></textarea>
+    <form onSubmit={handleSubmit}>
+      <textarea className="textarea textarea-bordered w-80 md:w-[32rem] mb-4" placeholder="Enter text here" onChange={(e) => handleChange(e)}></textarea>
+      <div>
+        <button className="btn btn-accent">Submit</button>
+      </div>
+    </form>
   )
 }
